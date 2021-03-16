30137aefb2658b515214614e7026ee19bdac06d3
José Armando Méndez Espinal <jamendez216@hotmail.com>
2021-03-15 21:46:47 -0400

    axios instaled

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer  clipped app v-model="drawer">
-      <v-list-item to="/" >
+    <v-navigation-drawer clipped app v-model="drawer">
+      <v-list-item to="/">
         <v-list-item-content>
           <v-list-item-title class="title">
             <v-list-item-action>
@@ -16,11 +16,11 @@
 
       <v-list>
         <v-list-group
-        v-for="item in Menu"
-        :key="item.title"
-        v-model="item.active"
-        :prepend-icon="item.action"
-        no-action
+          v-for="item in Menu"
+          :key="item.title"
+          v-model="item.active"
+          :prepend-icon="item.action"
+          no-action
         >
           <template v-slot:activator>
             <v-list-item-content>
@@ -28,8 +28,11 @@
             </v-list-item-content>
           </template>
 
-          <v-list-item v-for="submenu in item.submenu"
-            :key="submenu.title" v-bind:to="submenu.action">
+          <v-list-item
+            v-for="submenu in item.submenu"
+            :key="submenu.title"
+            v-bind:to="submenu.action"
+          >
             <v-list-item-content>
               <v-list-item-title v-text="submenu.title"></v-list-item-title>
             </v-list-item-content>
@@ -43,69 +46,99 @@
       <v-toolbar-title><h2>Inventario Batista</h2></v-toolbar-title>
     </v-app-bar>
 
-    <v-content>
+    <v-main>
       <v-container fluid fill-height>
         <v-slide-y-transition mode="out-in">
-          <router-view/>
+          <router-view />
         </v-slide-y-transition>
       </v-container>
-    </v-content>
+    </v-main>
 
+    <v-card width="100%">
+      <v-footer flat fixed absolute>
+        <v-card flat tile width="100%" class="text-center text-align-center">
+          <v-card-text>
+            <v-icon>work</v-icon>
+            <strong> jamendez216@gmail.com</strong> —
+            <v-icon>update</v-icon>
+            <a href="https://github.com/jamendez216">
+              github.com/jamendez216</a
+            >
+            —
+            <v-icon>call</v-icon>
+            809-654-6980 —
+            <v-icon>person</v-icon>
+            <a href="https://www.linkedin.com/in/jose-armando-mendez-espinal-23b2b7168/">
+            linkedin.com/josemendez/</a
+            >
+          </v-card-text>
+
+          <v-divider></v-divider>
+          <v-card-text>
+            {{ new Date().getFullYear() }} — <strong>Mendez' Solutions</strong>
+          </v-card-text>
+        </v-card>
+      </v-footer>
+    </v-card>
   </v-app>
 </template>
 
 <script>
-  export default {
-  name: 'App',
+export default {
+  name: "App",
   data: () => {
     return {
       clipped: false,
       drawer: true,
       fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
+      items: [
+        {
+          icon: "bubble_chart",
+          title: "Inspire",
+        },
+      ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js',
+      title: "Vuetify.js",
       Menu: [
         {
-          action: 'storefront',
+          action: "storefront",
           submenu: [
-                    { title: 'Categorías', action: 'categorias'}, 
-                    { title: 'Productos', action: 'productos'}
-                  ],
-          title: 'Almacén'
+            { title: "Categorías", action: "categorias" },
+            { title: "Productos", action: "productos" },
+          ],
+          title: "Almacén",
         },
         {
-          action: 'savings',
+          action: "savings",
           submenu: [
-                    { title: 'Ingresos', action: 'ingresos'}, 
-                    { title: 'Proveedores', action: 'proveedores'}
-                  ],
-          title: 'Compras'
+            { title: "Ingresos", action: "ingresos" },
+            { title: "Proveedores", action: "proveedores" },
+          ],
+          title: "Compras",
         },
         {
-          action: 'payment',
+          action: "payment",
           submenu: [
-                    { title: 'Ventas', action: 'ventas' }, 
-                    { title: 'Clientes', action: 'clientes'}
-                  ],
-          title: 'Ventas'
+            { title: "Ventas", action: "ventas" },
+            { title: "Clientes", action: "clientes" },
+          ],
+          title: "Ventas",
         },
         {
-          action: 'assignment_ind',
+          action: "assignment_ind",
           submenu: [
-                  { title: 'Usuarios', action: 'usuarios'}, 
-                  { title: 'Roles', action: 'roles'},
-                  ],
-          title: 'Accesos'
+            { title: "Usuarios", action: "usuarios" },
+            { title: "Roles", action: "roles" },
+          ],
+          title: "Accesos",
         },
-      ]
-    }
-  }
-}
+      ],
+      padless: false,
+      variant: "default",
+    };
+  },
+};
 </script>
 
--- src/components/Categoria.vue
+++ src/components/Categoria.vue
@@ -1,81 +1,294 @@
 <template>
-  <div>
-    <v-stepper v-model="e1">
-      <v-stepper-header>
-        <v-stepper-step :complete="e1 > 1" step="1">
-          Conoce a tu jevita
-        </v-stepper-step>
+  <v-layout align-start>
+    <v-flex>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :search="search"
+        sort-by="calories"
+        class="elevation-1"
+      >
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-toolbar-title><strong>Lista de Categorías</strong></v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialog" max-width="500px">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="primary"
+                  dark
+                  class="mb-2"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  New Item
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">{{ formTitle }}</span>
+                </v-card-title>
 
-        <v-divider></v-divider>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.name"
+                          label="Dessert name"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.calories"
+                          label="Calories"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.fat"
+                          label="Fat (g)"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.carbs"
+                          label="Carbs (g)"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.protein"
+                          label="Protein (g)"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
 
-        <v-stepper-step :complete="e1 > 2" step="2">
-          Sacala a pasear
-        </v-stepper-step>
-
-        <v-divider></v-divider>
-
-        <v-stepper-step step="3"> No se bro disculpa </v-stepper-step>
-      </v-stepper-header>
-
-      <v-stepper-items>
-        <v-stepper-content step="1">
-          <image id="imagen1" src="" />
-
-          <v-btn color="primary" @click="e1 = 2"> Continue </v-btn>
-
-          <v-btn text>Cancel</v-btn>
-        </v-stepper-content>
-
-        <v-stepper-content step="2">
-          <image id="imagen2" src="" />
-
-          <v-btn color="primary" @click="e1 = 3"> Continue </v-btn>
-
-          <v-btn text> Cancel </v-btn>
-        </v-stepper-content>
-
-        <v-stepper-content step="3">
-          <image id="imagen3" src="" />
-
-          <v-btn color="primary" @click="e1 = 1"> Continue </v-btn>
-
-          <v-btn text> Cancel </v-btn>
-        </v-stepper-content>
-      </v-stepper-items>
-    </v-stepper>
-  </div>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close">
+                    Cancel
+                  </v-btn>
+                  <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <v-dialog v-model="dialogDelete" max-width="400px">
+              <v-card>
+                <v-card-title class="headline"
+                  >Seguro de que quieres eliminar?</v-card-title
+                >
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="closeDelete"
+                    >Cancel</v-btn
+                  >
+                  <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                    >OK</v-btn
+                  >
+                  <v-spacer></v-spacer>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+        </template>
+        <template v-slot:[`item.actions`]="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)"> edit </v-icon>
+          <v-icon small @click="deleteItem(item)"> delete </v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize"> Reset </v-btn>
+        </template>
+      </v-data-table>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 export default {
-  data() {
+  data: () => ({
+    search: '',
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      {
+        text: "Dessert (100g serving)",
+        align: "start",
+        sortable: false,
+        value: "name",
+      },
+      { text: "Calories", value: "calories" },
+      { text: "Fat (g)", value: "fat" },
+      { text: "Carbs (g)", value: "carbs" },
+      { text: "Protein (g)", value: "protein" },
+      { text: "Actions", value: "actions", sortable: false },
+    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
+    defaultItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
+  }),
 
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "Crear Categoría" : "Editar Categoría";
+    },
   },
-  mounted() {
-    fetch(
-      "https://api.unsplash.com/photos/random/?client_id=J6_Hfxs_rnT0t5T6W0Edl6qMHVKSQwbFWC4hoWhczeg"
-    )
-      .then((res) => res.json())
-      .then((resJson) => {
-        console.log(resJson);
-        document.getElementById("imagen1").src = resJson.urls.raw;
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+  },
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      this.desserts = [
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+        },
+        {
+          name: "Ice cream sandwich",
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+        },
+        {
+          name: "Eclair",
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+        },
+        {
+          name: "Cupcake",
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+        },
+        {
+          name: "Gingerbread",
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+        },
+        {
+          name: "Jelly bean",
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+        },
+        {
+          name: "Lollipop",
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+        },
+        {
+          name: "Honeycomb",
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+        },
+        {
+          name: "Donut",
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+        },
+        {
+          name: "KitKat",
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+        },
+      ];
+    },
+
+    editItem(item) {
+      this.editedIndex = this.desserts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.desserts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+
+    deleteItemConfirm() {
+      this.desserts.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
       });
-    fetch(
-      "https://api.unsplash.com/photos/random/?client_id=J6_Hfxs_rnT0t5T6W0Edl6qMHVKSQwbFWC4hoWhczeg"
-    )
-      .then((res) => res.json())
-      .then((resJson) => {
-        console.log(resJson);
-        document.getElementById("imagen2").src = resJson.urls.raw;
+    },
+
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
       });
-    fetch(
-      "https://api.unsplash.com/photos/random/?client_id=J6_Hfxs_rnT0t5T6W0Edl6qMHVKSQwbFWC4hoWhczeg"
-    )
-      .then((res) => res.json())
-      .then((resJson) => {
-        console.log(resJson);
-        document.getElementById("imagen3").src = resJson.urls.raw;
-      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+      } else {
+        this.desserts.push(this.editedItem);
+      }
+      this.close();
+    },
   },
 };
 </script>
